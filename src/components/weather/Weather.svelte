484155e7d47c1be3e9b4321db66b39e9d4c22ea4
chunyukuo88 <alexgochenour@gmail.com>
2023-01-22 18:20:07 -0500

    Happy and unhappy paths for weather service. Updated test script, too. 100% coverage.

--- src/components/weather/Weather.svelte
+++ src/components/weather/Weather.svelte
@@ -4,10 +4,14 @@
   const url = `https://api.openweathermap.org/data/2.5/weather?q=Westerville,Ohio&appid=${process.env.WEATHER_API_KEY}`;
   let weatherString = 'Loading...';
   onMount(async () => {
-    const fetchedWeatherData = await fetch(url).then(res => res.json());
-    const temp = fetchedWeatherData.main.temp;
-    const humidity = fetchedWeatherData.main.humidity;
-    weatherString = `${temp}° at ${humidity}% humidity`;
+    try {
+      const fetchedWeatherData = await fetch(url).then(res => res.json());
+      const temp = fetchedWeatherData.main.temp;
+      const humidity = fetchedWeatherData.main.humidity;
+      weatherString = `${temp}° at ${humidity}% humidity`;
+    } catch (e) {
+      weatherString = 'Weather service unavailable';
+    }
   });
 </script>
 
--- src/components/weather/Weather.test.js
+++ src/components/weather/Weather.test.js
@@ -1,8 +1,11 @@
-import { describe, expect, test, vi } from 'vitest';
+import { afterEach, describe, expect, test, vi } from 'vitest';
 import Weather from './Weather.svelte';
 import { render, waitFor } from '@testing-library/svelte';
 import '@testing-library/jest-dom/extend-expect';
 
+afterEach(() => {
+  vi.clearAllMocks();
+});
 describe('GIVEN: The component renders,', ()=>{
   describe('WHEN: it is able to get weather data,', ()=>{
     test('THEN: it displays the temperature', async ()=>{
@@ -25,6 +28,21 @@ describe('GIVEN: The component renders,', ()=>{
       expect(weatherString).toBeVisible();
       await waitFor(() => {
         expect(weatherString.innerHTML).toStrictEqual('269° at 50% humidity');
+      });
+    });
+  });
+  describe('WHEN: it is NOT able to get weather data,', ()=>{
+    test('THEN: it displays the temperature', async ()=>{
+      global.fetch = vi.fn(() =>
+        Promise.reject(new Error('Service unavailable')),
+      );
+
+      render(Weather);
+      const weatherString = document.querySelector(`.weather-string`);
+
+      expect(weatherString).toBeVisible();
+      await waitFor(() => {
+        expect(weatherString.innerHTML).toStrictEqual('Weather service unavailable');
       });
     });
   });
