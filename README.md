90aabed941548434efde28276f9d14352f8d6d53
chunyukuo88 <alexgochenour@gmail.com>
2023-01-22 23:14:13 -0500

    Discovered 98.css

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 I have used SvelteKit professionally, but our team used version 0.9.x. Since the 1.0 release, enough has changed that I decided to make this little project to explore the changes. (There are many)
 
 This is a fully test-driven project.
+
+
+https://github.com/jdan/98.css
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,20 @@
+<head>
+  <title>98.css example</title>
+  <meta charset="UTF-8" />
+
+  <link rel="stylesheet" href="https://unpkg.com/98.css@0.1.4/build/98.css" />
+</head>
+
 <script>
   import Weather from '../components/weather/Weather.svelte';
 </script>
 
 <div class="app">
   <ul>
-    <li><a href="/about">about</a></li>
-    <li><a href="/blog">blog</a></li>
-    <li><a href="/games">games</a></li>
-    <li><a href="/admin">admin</a></li>
+    <li class="title-bar-text"><a href="/about">about</a></li>
+    <li class="title-bar-text"><a href="/blog">blog</a></li>
+    <li class="title-bar-text"><a href="/games">games</a></li>
+    <li class="title-bar-text"><a href="/admin">admin</a></li>
     <li><Weather /></li>
   </ul>
 </div>
